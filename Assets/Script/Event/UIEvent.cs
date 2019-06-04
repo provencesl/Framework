d4b88d7fcff0313b936769935e8efab764b8c4ab
provencesl <1457142838@qq.com>
2019-06-04 20:35:32 +0800

    first commit

---
+++ Assets/Script/Event/UIEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UIFramework
+{
+
+    public sealed class UIEvent
+    {
+        public static string WindowLoad()
+        {
+            return "windowLoad";
+        }
+    }
+        
+    
+
+}
---
+++ Assets/Script/UI/IUILife.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UIFramework
+{
+    /// <summary>
+    /// UI窗口生命周期管理
+    /// </summary>
+    public interface IUILife
+    {
+        GameObject GetGameObject { get; }
+
+        /// <summary>
+        /// 窗口初始化使用
+        /// </summary>
+        void OnInit();
+
+        /// <summary>
+        /// 窗口每次显示后调用
+        /// </summary>
+        void OnBeforeShow();
+
+        /// <summary>
+        /// 窗口每次显示后调用
+        /// </summary>
+        void OnShow();
+
+        /// <summary>
+        /// 窗口每帧渲染
+        /// </summary>
+        void OnUpdate();
+
+        /// <summary>
+        /// 窗口销毁调用
+        /// </summary>
+        void OnDestroy();
+
+
+    }
+}
+
+
